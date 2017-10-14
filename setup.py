1fa3f31768ac55175da170a2978265c2561ac854
Arun Chennadi <aruntejbombay@gmail.com>
2017-10-14 01:41:54 -0700

    Added url in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='arun-test',
     version='1.0.0',
     packages=['arun-package'],
-    url='',
+    url='https://github.com/ArunTejCh/arun-pypi-test',
     license='MIT',
     author='arunchennadi',
     author_email='aruntej@outlook.com',
