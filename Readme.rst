c2194be0c1e23b4a86463a404dcdf1f0076e5810
Arun Chennadi <aruntejbombay@gmail.com>
2017-10-14 01:36:50 -0700

    Initial commit

---
+++ Readme.rst
@@ -0,0 +1,4 @@
+An extremely awesome sentence. Another mega-awesome sentence.
+Lines that are together form a paragraph.
+
+Lines that are apart form separate paragraphs.
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='arun-test',
+    version='1.0.0',
+    packages=['arun-package'],
+    url='',
+    license='MIT',
+    author='arunchennadi',
+    author_email='aruntej@outlook.com',
+    description='A test package'
+)
